4b619c94f86b8aaca7f44dff71205b851e917975
Cong Chen <congchen5@gmail.com>
2015-03-09 22:45:54 -0700

    Can get a list of the tires from the costco site.

--- background.js
+++ background.js
@@ -4,7 +4,9 @@
 
 console.log('background.js loaded');
 
-var COSTCO_TIRES_WEBSITE = 'http://tires2.costco.com/SearchHandler.ashx';
+var COSTCO_TIRES_WEBSITE = 'http://tires2.costco.com/';
+var SEARCH_URL = 'SearchHandler.ashx';
+var SEARCH_RESULTS_URL = 'SearchResults.aspx';
 var DELIMITER = '@%@';
 
 // Config Options for the search.
@@ -16,37 +18,56 @@ var OPTIONS = 'Base';
 var jqxhr;
 var a;
 var b;
+var $foo;
 
 function getSearchUrl() {
-  return COSTCO_TIRES_WEBSITE + '?SearchParams=' + YEAR + DELIMITER + MAKE
-      + DELIMITER + MODEL + DELIMITER + OPTIONS + DELIMITER + '1';
+  return COSTCO_TIRES_WEBSITE + SEARCH_URL + '?SearchParams=' + YEAR +
+      DELIMITER + MAKE + DELIMITER + MODEL + DELIMITER + OPTIONS + DELIMITER
+      + '1';
 }
 
 function getSearchId() {
   console.log('getSearchId called');
   // Make inital search request.
-  jqxhr = $.ajax({
+  $.ajax({
       url: getSearchUrl(),
       type: 'GET',
       dataType: 'json'
     })
-    .fail(function(jqxhr, textStatus, error) {
-      console.log('Failure to get Costco website with error: ' + error);
+    .fail(function(error) {
+      console.log('Failure to get SearchId from Costco website. ' + error);
     })
     .done(function(data) {
       console.log('Finished getting search id.');
       makeSearchRequest(data[0]['SearchID']);
-      a = data;
     });
 };
 
 function makeSearchRequest(searchId) {
   console.log('makeSearchRequest called');
-  console.log(searchId);
-}
+  console.log(COSTCO_TIRES_WEBSITE + SEARCH_RESULTS_URL + '?SearchID='
+      + searchId);
+  $.ajax({
+      url: COSTCO_TIRES_WEBSITE + SEARCH_RESULTS_URL + '?SearchID=' + searchId,
+      type: 'GET'
+    })
+    .fail(function(error) {
+      console.log('Failure to get search results from Costco website. ' + error);
+    })
+    .done(function(data) {
+      console.log('Finished getting search results.');
+      var $temp = $.parseHTML(data);
+      //TODO handle pagination case.
+      $foo = $($temp[$temp.length - 1]).find('#p_lt_zoneContent_pageplaceholder_p_lt_ctl02_TireSearchResults_gvOptionalFitmentRearTires')
+          .find('div.tireDetails h3 span')
+          .map(function(index, el) {
+            return this.innerHTML;
+          });
+      console.log($foo);
+    });
+};
 
 getSearchId();
 console.log('done');
-
 //$foop = $('<form>' + data.results[0] + '</form>');
 //$('#content').add(data.results[0]);
